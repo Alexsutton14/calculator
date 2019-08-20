bf8efcce2dee4b9000c968a314188bc6c8010a0f
Alex Sutton <alexsutton14@hotmail.com>
2019-08-20 21:21:43 +0100

    started work on logic, added global variables

--- calc.js
+++ calc.js
@@ -8,10 +8,50 @@ const button7 = document.getElementById("7-button");
 const button8 = document.getElementById("8-button");
 const button9 = document.getElementById("9-button");
 const button0 = document.getElementById("0-button");
+const buttonDecimal = document.getElementById("decimal-point-button");
 
+//function buttons
 const buttonDivide = document.getElementById("divide-button");
 const buttonMultiply = document.getElementById("multiply-button");
 const buttonSubtract = document.getElementById("subtract-button");
 const buttonAdd = document.getElementById("add-button");
+
 const buttonEquals = document.getElementById("equals-button");
 
+const buttonClear = document.getElementById("clear-button");
+
+//active value variables
+let firstValue;
+let firstValueString;
+let secondValue;
+let secondValueString;
+let currentOperator = null;
+
+//display variables
+const displayLine1 = document.getElementById("display-line-1-value");
+const displayLine2 = document.getElementById("display-line-2-value");
+
+function clearSystem(){
+    firstValue = null;
+    firstValueString = null;
+    secondValue = null;
+    secondValueString = null;
+    currentOperator = null;
+}
+ function numberPressed(inputNumber){
+    let inputString = inputNumber.toString();
+    
+}
+
+button0.addEventListener("click", function(){numberPressed(0);});
+button1.addEventListener("click", function(){numberPressed(1);});
+button2.addEventListener("click", function(){numberPressed(2);});
+button3.addEventListener("click", function(){numberPressed(3);});
+button4.addEventListener("click", function(){numberPressed(4);});
+button5.addEventListener("click", function(){numberPressed(5);});
+button6.addEventListener("click", function(){numberPressed(6);});
+button7.addEventListener("click", function(){numberPressed(7);});
+button8.addEventListener("click", function(){numberPressed(8);});
+button9.addEventListener("click", function(){numberPressed(9);});
+
+buttonClear.addEventListener("click", function(){clearSystem();});
