f1321d1fc5f64858dfa95a3e8873c4893935b95b
Alex Sutton <alexsutton14@hotmail.com>
2019-08-20 20:56:44 +0100

    layout set up, started work on logic

---
+++ calc.js
@@ -0,0 +1,17 @@
+const button1 = document.getElementById("1-button");
+const button2 = document.getElementById("2-button");
+const button3 = document.getElementById("3-button");
+const button4 = document.getElementById("4-button");
+const button5 = document.getElementById("5-button");
+const button6 = document.getElementById("6-button");
+const button7 = document.getElementById("7-button");
+const button8 = document.getElementById("8-button");
+const button9 = document.getElementById("9-button");
+const button0 = document.getElementById("0-button");
+
+const buttonDivide = document.getElementById("divide-button");
+const buttonMultiply = document.getElementById("multiply-button");
+const buttonSubtract = document.getElementById("subtract-button");
+const buttonAdd = document.getElementById("add-button");
+const buttonEquals = document.getElementById("equals-button");
+
